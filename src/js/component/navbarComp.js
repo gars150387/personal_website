8a71f09466044605aa530c4c21067008ab8197b8
gars150387 <gar.santeliz@gmail.com>
2021-10-19 18:41:16 +0000

    setting up navbar options

--- src/js/component/navbarComp.js
+++ src/js/component/navbarComp.js
@@ -7,18 +7,18 @@ export const NavbarComp = () => {
 	return (
 		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand href="#home">Gustavo Rodriguez</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
+						<Nav.Link href="#features">About Me</Nav.Link>
+						<Nav.Link href="#pricing">Projects</Nav.Link>
 						<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
 							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
 							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
 							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
 							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.4">Certifications</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
 					<Nav>
