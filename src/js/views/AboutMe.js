185bf30acd539cc3914a468af456198ee13e8a4e
gars150387 <gar.santeliz@gmail.com>
2021-10-21 00:34:26 +0000

    loaded image

--- src/js/views/AboutMe.js
+++ src/js/views/AboutMe.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Row, Col, Container, Image } from "react-bootstrap";
 
 export const AboutMe = () => {
-	return <p>Hello World this is me!</p>;
+	return (
+		<>
+			<Container>
+				<Row>
+					<Col xs={6} md={4}>
+						<Image className="justify-content-end" src="gustavo-san-francisco.jpg" rouneded />
+					</Col>
+				</Row>
+			</Container>
+			<p>Hello World this is me!</p>
+		</>
+	);
 };
