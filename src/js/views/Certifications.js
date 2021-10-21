be615ff800b1b0c316fa1c794595b12042c25ebe
gars150387 <gar.santeliz@gmail.com>
2021-10-21 00:56:56 +0000

    added certifications

--- src/js/views/Certifications.js
+++ src/js/views/Certifications.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 export const Certifications = () => {
-	return <p>Hello World</p>;
+	return (
+		<Container>
+			<Row>
+				<Col xs={6} md={4}>
+					<Image src="Become_a_React_developer.PNG" rounded />
+				</Col>
+				<Col xs={6} md={4}>
+					<Image src="React_Building_an_Inteface.PNG" rounded />
+				</Col>
+				<Col xs={6} md={4}>
+					<Image src="React Components, Context, and Accessibility.PNG" rounded />
+				</Col>
+			</Row>
+		</Container>
+	);
 };
