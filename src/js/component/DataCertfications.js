a07261d26a5b8cb1c65b463d408d4402af4b3fd8
gars150387 <gar.santeliz@gmail.com>
2021-10-24 16:39:09 +0000

    still feeding file

--- src/js/component/DataCertfications.js
+++ src/js/component/DataCertfications.js
@@ -4,11 +4,16 @@ export const DataCertifications =[{
     company: "LinkedIn Learning",
     href: "https://www.linkedin.com/learning/paths/become-a-react-developer?u=107513458",
     img:"/BecomeAReactDeveloper.jpg"
-},
-{
+},{
     id:002,
     nameCertification: "React: Testing and Debugging",
     company: "LinkedIn Learning",
     href: "https://www.linkedin.com/learning/react-testing-and-debugging/testing-and-debugging-your-react-apps?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6820681459993583616&resume=true&u=107513458",
     img:"/ReactTestingAndDebugging.jpg"
+},{
+    id:003,
+    nameCertification: "Firebase Essential Training",
+    company: "LinkedIn Learning",
+    href: "https://www.linkedin.com/learning/firebase-essential-training/firebase-stop-reinventing-the-wheel?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6833404251495112704&resume=true&u=107513458",
+    img:"/FirebeEssentialTraining.jpg"
 },]
