a36fd7e0d7c7a27570f8e9c373664bf22f78b72c
gars150387 <gar.santeliz@gmail.com>
2021-10-24 16:35:48 +0000

    creating data file

---
+++ src/js/component/DataCertfications.js
@@ -0,0 +1,14 @@
+export const DataCertifications =[{
+    id:001,
+    nameCertification: "Become a React Developer",
+    company: "LinkedIn Learning",
+    href: "https://www.linkedin.com/learning/paths/become-a-react-developer?u=107513458",
+    img:"/BecomeAReactDeveloper.jpg"
+},
+{
+    id:002,
+    nameCertification: "Build a Google Map App With Javascript",
+    company: "LinkedIn Learning",
+    href: "https://www.linkedin.com/learning/paths/become-a-react-developer?u=107513458",
+    img:"/BecomeAReactDeveloper.jpg"
+},]
