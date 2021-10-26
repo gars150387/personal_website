8f5c31448e3284df7a948f299948043c6f9402e1
gars150387 <gar.santeliz@gmail.com>
2021-10-26 17:09:51 +0000

    feeding data

--- src/js/component/DataCertfications.js
+++ src/js/component/DataCertfications.js
@@ -16,4 +16,22 @@ export const DataCertifications =[{
     company: "LinkedIn Learning",
     link: "https://www.linkedin.com/learning/firebase-essential-training/firebase-stop-reinventing-the-wheel?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6833404251495112704&resume=true&u=107513458",
     img:"/FirebeEssentialTraining.jpg"
+},{
+    id:004,
+    title: "Node.js Essential Training",
+    company: "LinkedIn Learning",
+    link: "https://www.linkedin.com/learning/node-js-essential-training-web-servers-tests-and-deployment/go-beyond-the-basics-in-node?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6820681459993583616&resume=true&u=107513458",
+    img:"/FNode.jsEssentialTraining.jpg"
+},{
+    id:005,
+    title: "",
+    company: "LinkedIn Learning",
+    link: "",
+    img:"/.jpg"
+},{
+    id:006,
+    title: "",
+    company: "LinkedIn Learning",
+    link: "",
+    img:"/.jpg"
 },]
