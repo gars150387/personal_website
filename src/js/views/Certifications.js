78796450fb94ab40cd8b9c57a7db040e02bbfec9
gars150387 <gar.santeliz@gmail.com>
2021-10-27 17:38:19 +0000

    mapping data

--- src/js/views/Certifications.js
+++ src/js/views/Certifications.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { CardComp } from "../component/CardComp";
+import {DataCertifications} from "../component/DataCertfications";
 
 export const Certifications = () => {
 	return (
 		<>
-			<CardComp />
+		{DataCertifications && DataCertifications.map(()=>{
+			return (
+				<CardComp key={DataCertifications.id} DataCertifications={DataCertifications}/>
+			)
+		})}
+			
 		</>
 	);
 };
